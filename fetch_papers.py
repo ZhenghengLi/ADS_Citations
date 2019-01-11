237acd1d8a9ae7b3b351c8d0415cf608cf95b80b
Zhengheng Li <zhenghenge@gmail.com>
2019-01-12 00:44:26 +0800

    update fetch_papers.py

--- fetch_papers.py
+++ fetch_papers.py
@@ -20,7 +20,7 @@ def get_paper_info(url):
     result = {}
     result['title'] = soup.find("title").get_text()
     result['author_list'] = soup.find("meta", attrs = {"name": "citation_authors"}).get("content").split("; ")
-    result['doi'] = soup.find("meta", attrs = {"name": "citation_doi"}).get("content")
+    # result['doi'] = soup.find("meta", attrs = {"name": "citation_doi"}).get("content")
     result['bibcode'] = soup.find('input', attrs = {"type": "hidden", "name": "bibcode"}).get("value")
     dt_list = soup.find_all("dt")
     ref_citation = re.compile(r'.*Citations to the Article.*', re.I)
@@ -42,10 +42,9 @@ def get_citation_list(url):
     for name, link in paper_list:
         paper = get_paper_info(link)
         citation_list.append(paper)
+    return citation_list
 
-def check_citation_type(p1, p2):
-    al1 = p1['author_list']
-    al2 = p2['author_list']
+def check_citation_type(al1, al2):
     for x1 in al1:
         for x2 in al2:
             # TODO:
@@ -66,14 +65,23 @@ def write_data(dest_dir, status, main_paper, valid_citation_list):
         fout.writelines([line + "\n" for line in lines])
 
 
-def fetch_entry(paper_name_link):
-    name, link = paper_name_link
+def fetch_entry(link):
+    print "line 1"
     main_paper = get_paper_info(link)
+    print "line 2"
     author_list = main_paper['author_list']
+    print "line 3"
+    if not main_paper['citation_link']:
+        print "No citation for " + main_paper['bibcode']
+        return
     citation_list = get_citation_list(main_paper['citation_link'])
+    print "line 4"
     valid_citation_list = []
+    print "main_paper => " + main_paper['bibcode']
     for citation in citation_list:
-        if check_citation_type(author_list, citation['author_list']):
+        print "citation => " + citation['bibcode']
+        #if check_citation_type(author_list, citation['author_list']):
+        if True:
             valid_citation_list.append(citation)
     status = "%d / %d" % (len(valid_citation_list), len(citation_list))
     return status, main_paper, valid_citation_list
@@ -86,12 +94,18 @@ if __name__ == "__main__":
 
     url, destdir = sys.argv[1:3]
 
-    main_list = get_main_paper_list(url)
-    for name_link in main_list:
-        status, main_paper, valid_citation_list = fetch_entry(name_link)
-        write_data(destdir, status, main_paper, valid_citation_list)
+    #main_list = get_main_paper_list(url)
+
+    #for name_link in main_list[0]:
+    #    status, main_paper, valid_citation_list = fetch_entry(name_link)
+    #    write_data(destdir, status, main_paper, valid_citation_list)
 
     # res = get_paper_info(url)
     # for x in res:
     #     print x, "=>", res[x]
+
+    print fetch_entry(url)
+    # print get_citation_list(url)
+
+
 
