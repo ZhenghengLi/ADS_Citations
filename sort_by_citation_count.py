75c1822de1115f44a72d4f141aab340b0ac66a95
Zhengheng Li <zhenghenge@gmail.com>
2019-01-14 06:44:15 +0800

    add sort_by_citation_count.py

---
+++ sort_by_citation_count.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, sys, re, io
+import numpy as np
+
+def read_citation_count(filename):
+    first_line = None
+    with io.open(filename, 'r') as fin: first_line = fin.readline().strip()
+    ref_fl = re.compile(r'valid: (\d+); total: (\d+)')
+    m = ref_fl.match(first_line)
+    if m:
+        return int(m.group(2))
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+
+    bibcode_list = []
+    citation_count_list = []
+    for fn in sys.argv[1:]:
+        bibcode_list.append(fn[:-4])
+        citation_count_list.append(read_citation_count(fn))
+    bibcode_index = np.argsort(citation_count_list)
+    for x in reversed(bibcode_index):
+        print x, bibcode_list[x], citation_count_list[x]
+        idx_str = "%03d_" % (x + 1)
+        os.rename(bibcode_list[x] + ".txt", idx_str + bibcode_list[x] + ".txt")
+        os.rename(bibcode_list[x] + "_pdfs", idx_str + bibcode_list[x] + "_pdfs")
+
